809d6c00345c8485b5f3aadf1b5d9913c76ebaae
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2022-07-07 19:50:53 -0500

    CA-1-Listar

---
+++ src/main/java/net/javaguides/springboot/service/PersonaService.java
@@ -0,0 +1,36 @@
+package net.javaguides.springboot.service;
+
+import net.javaguides.springboot.interfaceService.IpersonaService;
+import net.javaguides.springboot.interfaces.IPersona;
+import net.javaguides.springboot.modelo.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonaService implements IpersonaService {
+    @Autowired
+    private IPersona data;
+
+    @Override
+    public List<Persona> listar() {
+        return (List<Persona>) data.findAll();
+    }
+
+    @Override
+    public Optional<Persona> listarId(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int save(Persona p) {
+        return 0;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
