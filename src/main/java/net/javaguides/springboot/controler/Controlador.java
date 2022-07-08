f2d6a7c1538390aedf85a9cb2a4b17f1c3c5374d
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2022-07-07 20:58:03 -0500

    CA-2-Agregar

--- src/main/java/net/javaguides/springboot/controler/Controlador.java
+++ src/main/java/net/javaguides/springboot/controler/Controlador.java
@@ -4,9 +4,12 @@ import net.javaguides.springboot.modelo.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,5 +24,17 @@ public class Controlador {
         List<Persona>personas=service.listar();
         model.addAttribute("personas", personas);
         return "index";
+    }
+
+    @GetMapping("/new")
+    public String agregar(Model model){
+        model.addAttribute("persona",new Persona());
+        return "form";
+    }
+
+    @PostMapping("/save")
+    public String save(@Valid Persona p, Model model){
+        service.save(p);
+        return "redirect:/listar";
     }
 }
